a6f2de2b6d1971a67e1d820851088d2ec9be45ad
midCenturyDude7 <mgriffes@gmail.com>
2025-03-29 09:12:16 -0700

    Minor update to text alignment

--- app/page.js
+++ app/page.js
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center space-y-4">
-      <h1 className="text-center text-3xl font-bold text-slate-300/70">
+      <h1 className="text-3xl font-bold text-slate-300/70">
         Framer Motion - Animated Tabs Exercise (Review)
       </h1>
     </div>
