978e267bb0460175d23f8ab7ba799bcf67ed94f8
midCenturyDude7 <mgriffes@gmail.com>
2025-04-05 11:14:47 -0700

    Added border and underline styling effects

--- app/components/Header/Navbar/Navbar.jsx
+++ app/components/Header/Navbar/Navbar.jsx
@@ -36,9 +36,9 @@ export default function Navbar() {
                   // onPointerLeave={() => setFocused(null)}
                   tabIndex={0}
                   className={cn(
-                    "btn-tab h-[2.5rem] w-[5.5rem] rounded-lg bg-transparent px-8 text-slate-300/70",
+                    "btn-tab transtion-colors h-[2.5rem] w-[5.5rem] rounded-lg border border-transparent px-8 text-slate-300/70 duration-1000 ease-in-out hover:border-[#00b7ff27] hover:text-gray-200/80",
                     selected === path
-                      ? "cursor-default rounded-lg bg-gradient-to-b from-[#000208] to-[#141449]"
+                      ? "cursor-default rounded-lg bg-gradient-to-b from-[#000208] to-[#141449] transition-colors duration-1000 ease-in-out border-[#00b7ff31] text-gray-200/80"
                       : "",
                   )}
                 >
