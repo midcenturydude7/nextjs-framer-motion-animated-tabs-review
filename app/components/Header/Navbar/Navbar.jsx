b121f7deed86c16c6d052b1c58b7da9ec0c6a7e4
midCenturyDude7 <mgriffes@gmail.com>
2025-04-08 21:34:38 -0700

    Fine-tuning boomerang animated tab settings

--- app/components/Header/Navbar/Navbar.jsx
+++ app/components/Header/Navbar/Navbar.jsx
@@ -52,8 +52,8 @@ export default function Navbar() {
                   <AnimatePresence>
                     {focused === path ? (
                       <motion.div
-                        initial={{ opacity: 0 }}
-                        animate={{ opacity: 1 }}
+                        // initial={{ opacity: 0 }}
+                        // animate={{ opacity: 1 }}
                         exit={{
                           opacity: 0,
                           transition: {
